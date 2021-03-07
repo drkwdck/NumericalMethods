6153bbd3afc16f54294114bf325f1d7a93a01eb1
drkwdck <reshetnikov.e.a@mail.ru>
2021-03-07 13:17:38 +0300

    добавлено построение графиков и комменты

---
+++ 3/scripts/main.m
@@ -0,0 +1,36 @@
+clear
+clc
+u0 = [1 1];
+t = linspace(0, 0.75, 8);
+alphas =[0.5 1 (1+i)/2];
+
+figure(1)
+hold on
+grid on
+% массив кривых на графике
+plots = [];
+% массив подписей для каждого элемента из массива plots
+plots_lables = strings(size(alphas, 2) * 2);
+
+for j=1:3
+    % считаем значения для u
+    u = rozenbrok(u0, alphas(j), t);
+
+    % так как u - двумерная, строим график для каждой компоненты отдельно
+    u1 = plot(t, u(:,1), '--');
+    u2 = plot(t, u(:,2), '--');
+    
+    % формируем подписи для кривых на графике (легенда)
+    label1 = "rozenbork(1) " + alphas(j);
+    label2 = "rozenbork(2) " + alphas(j);
+    plots(j + j - 1) = u1;
+    plots(j + j) = u2;
+    plots_lables(j + j -1) = label1;
+    plots_lables(j + j) = label2;
+end
+
+% выводим легенду
+legend(plots, plots_lables)
+
+
+
