94ca3fdcdf3855639cb4412b711653af08ae6308
drkwdck <reshetnikov.e.a@mail.ru>
2021-03-10 23:55:06 +0300

    исправлена вторая лаба

--- 2/scripts/script2_2.m
+++
@@ -1,6 +0,0 @@
-a = zeros(7,3)
-
-for i = 1:7
-    b = runge_kutta('test_function_1', 0, 1, [1;-0.5;0.6], 2^(i-1))
-end
-
--- 2/scripts/task_4.m
+++ 2/scripts/task_4.m
@@ -6,8 +6,8 @@ t_end = 1;
 last_node_u = zeros(7,1);
 figure(1)
 for i = 1:7
-    [t, u] = runge_kutta('test_function_1', t_start, t_end, u0, 2^(i - 1));
-    last_node_u(i) = u(length(u));
+    [t, u] = runge_kutta_unviersal('test_function_1', t_start, t_end, u0, 2^(i - 1));
+    last_node_u(i) = u(length(t));
     plot(t, u)
     title('u(t) для разного количества узлов сетки')
     xlabel('t')
